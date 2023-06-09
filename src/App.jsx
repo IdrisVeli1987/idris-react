deeacf59e4104b210a019db10d6a94db4407bb22
IdrisVeli1987 <idrisveliev@gmail.com>
2023-06-10 01:38:45 +0400

    add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-function Codes({ ad, yas }) {
-  console.log(ad);
+function App() {
   return (
     <div>
-      <p>
-        Hi. I'm {ad} and {yas} years old. I love programming in JS and React
-      </p>
-      <img src="https://turbo.azstatic.com/uploads/full/2023%2F05%2F17%2F16%2F38%2F41%2F774a2652-e936-45d6-b554-ca532244e9c8%2F15351_c39X6BzCkfgVz_yjo9zUSg.jpg" />
+      <p>I love programming. Its my dream!!</p>
     </div>
   );
 }
-export default Codes;
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
-import Codes from "./App";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+function Codes() {
+  console.log("salam");
+  return (
+    <div>
+      <p>Hi, i'm Idris. Programming its very super</p>
+      <img src="https://www.onlc.com/blog/wp-content/uploads/2017/07/ONLC-2017-4.png" />
+    </div>
+  );
+}
+
 root.render(
   <div>
-    <Codes ad="Idris" yas={36} />
-    <h3>Men React oyrenirem</h3>
+    {Codes()}
+    {App()}
+    <p>I'm learning React</p>
   </div>
 );
