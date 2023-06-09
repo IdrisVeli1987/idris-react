5a96d6b37d96a782cec8fb3bbd89d845414dc8b3
IdrisVeli1987 <idrisveliev@gmail.com>
2023-06-10 01:47:30 +0400

    add Codes.jsx

---
+++ src/Codes.jsx
@@ -0,0 +1,10 @@
+export function Codes() {
+    console.log("salam");
+    return (
+      <div>
+        <p>Hi, i'm Idris. Programming its very super</p>
+        <img src="https://www.onlc.com/blog/wp-content/uploads/2017/07/ONLC-2017-4.png" />
+      </div>
+    );
+  }
+  
--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
 import App from "./App";
+import { Codes } from "./Codes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Codes() {
-  console.log("salam");
-  return (
-    <div>
-      <p>Hi, i'm Idris. Programming its very super</p>
-      <img src="https://www.onlc.com/blog/wp-content/uploads/2017/07/ONLC-2017-4.png" />
-    </div>
-  );
-}
 
 root.render(
   <div>
