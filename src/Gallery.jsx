d59c0ff50c06a9e57229bca711ac631308c2893c
IdrisVeli1987 <idrisveliev@gmail.com>
2023-06-10 14:15:36 +0400

    ana foto

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,11 +1,6 @@
 import { useState } from "react";
 
-const images = [
-  "https://images.unsplash.com/photo-1592289701772-4a4a8949f8ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
-  "https://images.unsplash.com/photo-1549082984-1323b94df9a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
-  "https://images.unsplash.com/photo-1531496730074-83b638c0a7ac?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1548&q=80",
-  "https://images.unsplash.com/photo-1508739826987-b79cd8b7da12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1546&q=80",
-];
+const images = ["O1.jpg", "O2.jpg", "O3.jpg", "O4.jpg", "O5.jpg"];
 const Gallery = () => {
   const [index, setIndex] = useState(0);
   const pre = () => {
@@ -24,14 +19,10 @@ const Gallery = () => {
   };
   return (
     <>
-      <img src={images[index]} alt="" />
+      <img src={"/images/" + images[index]} alt="" />
       <div className="icons">
-        <button onClick={pre}>
-          {"<"}
-        </button>
-        <button onClick={next}>
-          {">"}
-        </button>
+        <button onClick={pre}>{"<"}</button>
+        <button onClick={next}>{">"}</button>
       </div>
     </>
   );
