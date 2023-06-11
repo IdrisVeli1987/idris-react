5293c457362fc6a5ee642f55fc1d490d020b6f18
IdrisVeli1987 <idrisveliev@gmail.com>
2023-06-11 15:34:51 +0400

    add page, Users file and data

---
+++ src/page/Users/index.jsx
@@ -0,0 +1,10 @@
+import { users } from "./data";
+
+const Users = () => {
+  return (
+        <>
+            <h1>İstifadəçi səhifəsinə xoş gəlmisiz</h1>
+        </>
+    );
+};
+export default Users;
